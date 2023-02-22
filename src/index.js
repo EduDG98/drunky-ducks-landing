fa5f3069e1e486731af687861d2278201d777588
Juan Felipe Ramirez <juanfel2024@gmail.com>
2023-02-22 13:06:04 +0100

    Implementado fetch para recuperar datos JSON
    Creado un archivo JS con funcion fetch para recuperar
    los datos de las bebidas del cocktails.json

--- src/index.js
+++ src/index.js
@@ -1,25 +1,30 @@
-let quotesArray = [
-    'Después de tragos y fiesta, mira bien con quien te acuestas.',
-    'El borracho valiente, se pasa del vino al aguardiente.',
-    'Yo no tengo problemas con la bebida, excepto cuando no puedo servirme una'
-]
+import getAllCocktails from "./js/cocktails";
 
-let phraseContainer = document.getElementsByClassName('random-quotes')[0]
+const quotesArray = [
+  "Después de tragos y fiesta, mira bien con quien te acuestas.",
+  "El borracho valiente, se pasa del vino al aguardiente.",
+  "Yo no tengo problemas con la bebida, excepto cuando no puedo servirme una"
+];
 
-phraseContainer.innerHTML = quotesArray[Math.floor(Math.random()*quotesArray.length)];
+const phraseContainer = document.getElementsByClassName("random-quotes")[0];
 
+phraseContainer.innerHTML = quotesArray[Math.floor(Math.random() * quotesArray.length)];
 
-let buttons = document.querySelectorAll('.button')
+const buttons = document.querySelectorAll(".button");
 
-for (let button of buttons) {
-    button.addEventListener('click', (event) => {
-        let choose = event.target.classList[1]
-        let modal = document.querySelector('.modal')
+for (const button of buttons) {
+  button.addEventListener("click", (event) => {
+    const choose = event.target.classList[1];
+    const modal = document.querySelector(".modal");
 
-        if (choose === 'overage') {
-            modal.style.display = 'none'
-        } else {
-            location.replace('https://www.google.com/')
-        }
-    })
+    if (choose === "overage") {
+      modal.style.display = "none";
+    } else {
+      location.replace("https://www.google.com/");
+    }
+  });
 }
+
+const datos = await getAllCocktails();
+
+console.log(datos);
