c9ff687be5c4dbc0806e5c179bf12bf6a6a81be7
Juan Felipe Ramirez <juanfel2024@gmail.com>
2023-02-22 12:25:13 +0100

    Funcionalidad carrousel con Swiper.js
    Hemos agregado un carrousel con la ayuda de una libreria,
    llamada Swiper.js. Para transicionar entre unos cocteles
    de ejemplo para el usuario.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,58 @@
-let quotesArray = [
-    'Después de tragos y fiesta, mira bien con quien te acuestas.',
-    'El borracho valiente, se pasa del vino al aguardiente.',
-    'Yo no tengo problemas con la bebida, excepto cuando no puedo servirme una'
-]
+import Swiper, { Navigation, Pagination, Autoplay } from "swiper";
+// import Swiper and modules styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
-let phraseContainer = document.getElementsByClassName('random-quotes')[0]
+const quotesArray = [
+  "Después de tragos y fiesta, mira bien con quien te acuestas.",
+  "El borracho valiente, se pasa del vino al aguardiente.",
+  "Yo no tengo problemas con la bebida, excepto cuando no puedo servirme una"
+];
 
-phraseContainer.innerHTML = quotesArray[Math.floor(Math.random()*quotesArray.length)];
+const phraseContainer = document.getElementsByClassName("random-quotes")[0];
 
+phraseContainer.innerHTML = quotesArray[Math.floor(Math.random() * quotesArray.length)];
 
-let buttons = document.querySelectorAll('.button')
+const buttons = document.querySelectorAll(".button");
 
-for (let button of buttons) {
-    button.addEventListener('click', (event) => {
-        let choose = event.target.classList[1]
-        let modal = document.querySelector('.modal')
+for (const button of buttons) {
+  button.addEventListener("click", (event) => {
+    const choose = event.target.classList[1];
+    const modal = document.querySelector(".modal");
 
-        if (choose === 'overage') {
-            modal.style.display = 'none'
-        } else {
-            location.replace('https://www.google.com/')
-        }
-    })
+    if (choose === "overage") {
+      modal.style.display = "none";
+    } else {
+      location.replace("https://www.google.com/");
+    }
+  });
 }
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  modules: [Navigation, Pagination, Autoplay],
+  direction: "horizontal",
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
